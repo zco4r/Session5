977b5312ec57bf8e1a35be9fcc0e887fc728f620
ammaralazki <alazkiyasser3@gmail.com>
2026-07-09 09:49:09 +0400

    Task 1: Fixed Grades Array

--- Session5/Program.cs
+++ Session5/Program.cs
@@ -3,7 +3,20 @@
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    { 
+        //Task 1: Fixed Grades Array
+        int[] grades = new int[5];
+
+        for (int i = 0; i < grades.Length; i++) {
+            Console.Write($"Enter grade {i + 1}: ");
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Grades entered:");
+        foreach (int grade in grades) {
+            Console.WriteLine(grade);
+        }
+        
+    
     }
 }
